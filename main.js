c4e5606a382df25637b89443fef401b36ce1baba
k20317 <k20726tm@gmail.com>
2021-07-23 12:45:19 +0900

    ポートフォリオ途中まで

---
+++ main.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("TMIdisplay");
+const button = document.getElementById("TMIbutton");
+
+const display2 = document.getElementById("fromdisplay");
+const button2 = document.getElementById("frombutton");
+
+const display3 = document.getElementById("agedisplay");
+const button3 = document.getElementById("agebutton");
+
+//TMIButtonを押した後の処理
+TMIbutton.onclick = function () {
+  TMIdisplay.textContent = "趣味でけん玉をはじめました♪";
+};
+
+//fromButtonを押した後の処理
+frombutton.onclick = function () {
+  fromdisplay.textContent = "Fukuoka";
+};
+
+//nameButtonを押した後の処理
+agebutton.onclick = function () {
+  agedisplay.textContent = "21";
+};
