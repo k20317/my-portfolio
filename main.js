c49266782229890645d8495826e8261cc62ced1c
k20317 <k20726tm@gmail.com>
2021-07-25 17:50:33 +0900

    ポートフォリオ追加

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const display = document.getElementById("TMIdisplay");
 const button = document.getElementById("TMIbutton");
 
+const display0 = document.getElementById("TMI_display");
+const button0 = document.getElementById("TMI_button");
+
 const display2 = document.getElementById("fromdisplay");
 const button2 = document.getElementById("frombutton");
 
@@ -10,6 +13,11 @@ const button3 = document.getElementById("agebutton");
 //TMIButtonを押した後の処理
 TMIbutton.onclick = function () {
   TMIdisplay.textContent = "最近、けん玉をはじめました！";
+};
+
+//TMI_Buttonを押した後の処理
+TMI_button.onclick = function () {
+  TMI_display.textContent = "オリンピック全力応援♪";
 };
 
 //fromButtonを押した後の処理
